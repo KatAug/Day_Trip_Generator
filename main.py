53b9ebc424b5a3f33d1d3325b11d36ea109df756
Katrina Augustine <katrinaaugustine2022@gmail.com>
2022-11-30 14:33:26 -0600

    Added lists and dictionary for Day Trip Generator

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello World")
+#print("Hello World")
+
+#As a developer, I want to make at least three commits with descriptive messages
+
+#As a developer, I want to store my destinations, restaurants, mode of transportation, and entertainment in their own separate lists.
+
+destinations = ["Galveston Island", "Jacob's Well", "Six Flags Fiesta", "Dinosaur Valley State Park", "Bracken Cave"]
+restaurants = ["Whataburger", "Chili's", "Texas Roadhouse", "Chick-fil-A", "Firehouse Subs"]
+mode_of_transportation = ["Car", "Bus", "Train", "Motorcycle", "Helicopter"]
+entertainment = ["Bingo", "Dancing", "Karaoke", "Bouncy Castle", "Comedy Club"]
+
+#As a developer, I want to store my final day trip selections in a Dictionary, with a unique key value pair for each piece of my day trip. 
+
+my_day_trip_dictionary = {"destination":[destinations], "restaurant": [restaurants], "transportation": [mode_of_transportation], "thing to do":[entertainment]}
+#print(my_day_trip_dictionary)
+
+
+
+ 
