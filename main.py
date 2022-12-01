accea3d5623e9b90ae510ca4cc3a3786243d52ee
Katrina Augustine <katrinaaugustine2022@gmail.com>
2022-12-01 07:29:24 -0600

    Created functions to select random destination, restaurant, transportation, and entertainment) for user

--- main.py
+++ main.py
@@ -6,15 +6,38 @@ restaurants = ["Whataburger", "Chili's", "Texas Roadhouse", "Chick-fil-A", "Fire
 mode_of_transportation = ["Car", "Bus", "Train", "Motorcycle", "Helicopter"]
 entertainment = ["Bingo", "Dancing", "Karaoke", "Bouncy Castle", "Comedy Club"]
 
+
 pick_destination = random.choice(destinations)
+def destination():
+    print(pick_destination)
+destination()
+
 pick_restaurant = random.choice(restaurants)
-pick_mode_mode_of_transportation = random.choice(mode_of_transportation)
+def restaurant():
+    print(pick_restaurant)
+restaurant()
+
+pick_mode_of_transportation = random.choice(mode_of_transportation)
+def transportation():
+    print(pick_mode_of_transportation)
+transportation()
+
 pick_entertainment = random.choice(entertainment)
-#print(destinations, restaurants, mode_of_transportation, entertainment)
-print(pick_destination)
-print(pick_restaurant)
-print(pick_mode_mode_of_transportation)
-print(pick_entertainment)
+def entertainment():
+    print(pick_entertainment)
+entertainment()
+
+
+#pick_destination = random.choice(destinations)
+#pick_restaurant = random.choice(restaurants)
+#pick_mode_of_transportation = random.choice(mode_of_transportation)
+#pick_entertainment = random.choice(entertainment)
+
+
+#print(pick_destination)
+#print(pick_restaurant)
+#print(pick_mode_of_transportation)
+#print(pick_entertainment)
 
 
 
@@ -45,4 +68,13 @@ my_day_trip_dictionary = {"destination":[destinations], "restaurant": [restauran
 
 #As a user I want a form of entertainment to be randomly selected for my day trip. 
 #pick_entertainment = random.choice(entertainment)
+
+#As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don't like one or more of those things. 
+
+#As a user, I want to be able to confirm that my day trip is "complete" once I like all of the random selections. 
+
+#As a user, I want to display my completed trip in the console. 
+
+#Single Responsibility: As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!
+
 
