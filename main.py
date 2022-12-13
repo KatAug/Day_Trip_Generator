5afae7fd457bf474f1c98b2a52360d0aa76eb506
Katrina Augustine <katrinaaugustine2022@gmail.com>
2022-12-13 17:51:44 -0600

    Added function so user could confirm day trip

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 
 import random
 
-                            #0                #1                #2                     #3                       #4
+
 destination_list = ["Galveston Island", "Jacob's Well", "Six Flags Fiesta", "Dinosaur Valley State Park", "Bracken Cave"]
-                        #0            #1            #2                 #3              #4
 restaurant_list = ["Whataburger", "Chili's", "Texas Roadhouse", "Chick-fil-A", "Firehouse Subs"]
-                                #0      #1      #2         #3           #4
 mode_of_transportation_list = ["Car", "Bus", "Train", "Motorcycle", "Helicopter"]
-                        #0          #1          #2            #3              #4
 things_to_do_list = ["Hiking", "Bicycling", "Dancing", "Singing Karaoke", "Swimming"]
 
-my_day_trip_dictionary = {"1": "Galveston Island", 
-"2": "Jacob's Well", "3": "Six Flags Fiesta", "4": "Dinosaur Valley State Park", "5": "Bracken Cave", 
-"6": "Whataburger", "7": "Chili's", "8": "Texas Roadhouse","9": "Chick-Fil-A", "10": "Firehouse Subs",
-"11":"bus", "12":"car", "13": "train", "14": "motorcycle", "15": "helicopter",
-"16": "dancing", "17":"hiking", "18": "bicycling", "19": "singing Karaoke", "20": "swimming"}
+my_day_trip_dictionary = {
+"Destination": "pick_destination",
+"Restaurant": "pick_restaurant", 
+"Transportation": "pick_mode_of_transportation", 
+"Entertainment": "pick_things_to_do"}
 
 def greeting():
     print("")
@@ -32,12 +29,12 @@ def picked_destination(list):
         if user_input == "y":
             print(f"{pick_destination} will be great!")
             print("")
-        if user_input == "n":
-            print("Let's try again!")
-            print("")
-        else:
+            my_day_trip_dictionary["Destination"] = pick_destination
             confirm_bool = False
- 
+        else: 
+            print("Let's try again!")
+            print("") 
+
 def picked_restaurant(list):
     confirm_bool = True
     while confirm_bool == True:
@@ -47,11 +44,11 @@ def picked_restaurant(list):
         if user_input == "y":
             print(f"{pick_restaurant} will be awesome!")
             print("")
-        if user_input == "n":
+            my_day_trip_dictionary["Restaurant"] = pick_restaurant
+            confirm_bool = False
+        else:
             print("Let's try again!")
             print("")
-        else:
-            confirm_bool = False 
 
 def picked_mode_of_transportation(list):
     confirm_bool = True
@@ -62,12 +59,12 @@ def picked_mode_of_transportation(list):
         if user_input == "y":
             print(f"Traveling via {pick_mode_of_transportation} will be perfect!")
             print("")
-        if user_input == "n":
+            my_day_trip_dictionary["Transportation"] = pick_mode_of_transportation
+            confirm_bool = False
+        else:
             print("Let's try again!")
             print("")
-        else:
-            confirm_bool = False
-
+            
 def picked_things_to_do(list):
     confirm_bool = True
     while confirm_bool == True:
@@ -77,25 +74,27 @@ def picked_things_to_do(list):
         if user_input == "y":
             print(f"{pick_things_to_do} will be a lot of fun!")
             print("")
-        if user_input == "n":
+            my_day_trip_dictionary["Entertainment"] = pick_things_to_do
+            confirm_bool = False
+        else:
             print("Let's try again!")
             print("")
-        else:
-            confirm_bool = False
-#Need to confirm that my day trip is "complete" once I like all of the random selections.
 def confirmation():
-    confirm = False
-    while confirm == False:
-        user_input = input(f"Does traveling to {picked_destination} via {picked_mode_of_transportation}, eating at {picked_restaurant} and {picked_things_to_do} sound like the perfect day trip? (y/n): ")
-        print("")
-    if user_input == "y":
-        print(f"You have confirmed that you will be traveling to {picked_destination} via {picked_mode_of_transportation}, eating at {picked_restaurant} and {picked_things_to_do}! Have a great time on your day trip!")
-        print("")
-    if user_input == "n":
-        print("What would you like to change?")
-    else: 
-        confirm = True
-
+    confirm_bool = True
+    while confirm_bool == True:
+        print(f"You will be traveling to {my_day_trip_dictionary['Destination']} via {my_day_trip_dictionary['Transportation']}, eating at {my_day_trip_dictionary['Restaurant']} and {my_day_trip_dictionary['Entertainment']}!")
+        print("") 
+        user_input = input("Are you satisfied with this day trip? (y/n)")
+        if user_input == "y":
+            print("")
+            print("Have a wonderful time!")
+            print("") 
+            confirm_bool = False  
+        else:
+            print("Let's try again!")
+            print("")
+            
+               
 greeting()
 picked_destination(destination_list)
 picked_restaurant(restaurant_list)
@@ -103,7 +102,4 @@ picked_mode_of_transportation(mode_of_transportation_list)
 picked_things_to_do(things_to_do_list)
 confirmation()
  
-#want to store my final day trip selections in a Dictionary, with a unique key value pair for each piece of my day trip.
-# for key, value in my_day_trip_dictionary.items():
-#     print(f"{key}: {value}")
 
